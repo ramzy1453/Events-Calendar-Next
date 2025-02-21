2423d4344380dcf378739ef1b5a828ce3b00b695
ramzy1453 <kemmounramzy93@gmail.com>
2025-02-21 22:45:37 +0100

    feat: add events integration

--- app/(home)/rooms/page.tsx
+++ app/(home)/rooms/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function EventsPage() {
-  return <div className="flex-[3] border p-4 space-y-4">Nothing</div>;
+  return <div className="flex-[3] border p-4 space-y-4">Events None</div>;
 }
